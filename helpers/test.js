9d89fcc6ffde1885b0f61ff0367b18dcea266ccd
Fadi Badawi <ifadbad@hotmail.com>
2017-11-06 19:13:27 +0200

    update 1.0.4
    update 1.0.4

---
+++ helpers/test.js
@@ -0,0 +1,8 @@
+const options = {};
+
+// options.ip = '127.0.0.1';
+// options.port = parseInt(process.argv[2]);
+// options.config = { name: 'BWNode.js' };
+// options.sleep = 3000;
+
+require('bwnode.js').http('test', options);
