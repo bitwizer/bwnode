9422be30dcb24d7a5ffa72b7d4b441105540788d
Fadi Badawi <ifadbad@hotmail.com>
2017-11-06 18:59:40 +0200

    1.0.3
    1.0.3 update, add sqlagent

--- __old/helpers/release.js
+++
@@ -1,9 +0,0 @@
-const options = {};
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'BWNode.js' };
-// options.sleep = 3000;
-
-require('bwnode.js').http('release', options);
-// require('bwnode.js').cluster.http(5, 'release', options);
--- __old/helpers/test.js
+++
@@ -1,8 +0,0 @@
-const options = {};
-
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
-// options.config = { name: 'BWNode.js' };
-// options.sleep = 3000;
-
-require('bwnode.js').http('test', options);
